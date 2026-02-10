6d1aa98c3ef24d90c78a7d00264d465161084857
Nitesh Kumar Samota <samotanitesh247@gmail.com>
2026-02-10 17:37:48 +0530

    message route is completed now we create the UI

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 
 dotenv.config();
 const app = express();
@@ -15,6 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoutes);
 
 app.listen(PORT,()=>{
     console.log("server is running on PORT : "+ PORT);
